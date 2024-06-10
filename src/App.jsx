64c01f560485fe40ba98f609ec9244045ab2a39f
Maks12-droid <127047393+Maks12-droid@users.noreply.github.com>
2024-06-10 15:21:41 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -20,10 +20,7 @@ const App = () => {
         location={user.location}
         stats={user.stats}
       />
-
-      <Statistics title="Upload stats" stats={statisticalData} />
-      <Statistics stats={statisticalData} />
-
+      
       <FriendList friends={friends} />
 
       <TransactionHistory items={transactions} />
